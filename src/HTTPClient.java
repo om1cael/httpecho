ee28a8891d79ff764ed95f13eb45bf5e914dea67
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-18 21:09:53 -0300

    setup httpclient request

---
+++ src/HTTPClient.java
@@ -0,0 +1,13 @@
+public class HTTPClient {
+    final static String request =
+        """
+        GET /index.html HTTP/1.1
+        Host: example.com
+        User-Agent: ExampleClient/1.0
+        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+        """;
+
+    public static void main(String[] args) {
+
+    }
+}
